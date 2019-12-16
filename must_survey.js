ce33fbe77489c5812d4c794dcd45dfcd8af59d1b
pwxssj <2427804139@qq.com>
2019-12-16 19:23:26 +0800

    Delete must_survey.js

--- must_survey.js
+++
@@ -1,7 +0,0 @@
-var jq = document.createElement('script');
-jq.setAttribute('src', 'https://code.jquery.com/jquery-3.4.1.min.js');
-document.getElementsByTagName('head')[0].appendChild(jq);
-
-function checkAll() {
-	$("input[name^='sq'][value='5']").each(function() {$(this).attr("checked", "checked")});
-}
